62f54b797601b2ff66aa67f6b55caf56eca45f8d
Leon-Zmk <leonzmkdev@gmail.com>
2022-07-12 00:06:41 +0630

    item_details_and_vendors_shops

---
+++ resources/views/frontend/vendor_detail.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.frontend.frontend_layout')
+
+
+@section('content')
+
+    <div class="container">
+       <div class="row">
+        
+       </div>
+    </div>
+
+
+
+
+   <!-- Back to Top -->
+   <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+@endsection
---
+++ resources/views/frontend/vendors_page.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.frontend.frontend_layout')
+
+
+@section('content')
+
+    <div class="container">
+       <div class="row">
+         @foreach ($vendors as $vendor)
+          <div class="col-12 col-md-3">
+            <div class="product-item  bg-light">
+               <div class="product-img  position-relative overflow-hidden">
+                   <img class="img-fluid w-100" src="{{asset("images/frontend_img/img/product-1.jpg")}}" alt="">
+                   <div class="product-action">
+                     
+                       <a class="btn btn-outline-dark btn-square" href="{{route("vendordetail",$vendor->id)}}"><i class="fas fa-info"></i></a> 
+                   </div>
+               </div>
+               <div class="text-center py-4">
+                   <a class="h6 text-decoration-none text-truncate" href="">{{$vendor->shop_name}}</a>
+               </div>
+           </div>
+          
+          </div>
+          @endforeach
+       </div>
+    </div>
+
+
+
+
+   <!-- Back to Top -->
+   <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+@endsection
